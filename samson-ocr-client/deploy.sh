7f7b33878574513fca9e1f64fd834584ae3b6a05
Ken Nguyen <ikendoit1998@gmail.com>
2020-03-28 15:37:34 +0700

    updated distribution

--- samson-ocr-client/deploy.sh
+++ samson-ocr-client/deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 source /home/ken/.bashrc
+source ./.env
 
 # build and push to s3
 npm run build && aws s3 sync build/ s3://samson-ocr-web --delete
 
 # invalidate cloudfront distribution
-aws cloudfront create-invalidation --distribution-id=E397LJ178VB74Y --paths "/*"
+aws cloudfront create-invalidation --distribution-id=$DISTRIBUTION_ID --paths "/*"
