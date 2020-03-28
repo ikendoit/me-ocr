dda7eaf57d3dad2adf4483c7d5416c3c6f9d6f3d
Ken Nguyen <ikendoit1998@gmail.com>
2020-03-28 15:39:01 +0700

    added comments to deploy script

--- samson-ocr-client/deploy.sh
+++ samson-ocr-client/deploy.sh
@@ -3,8 +3,8 @@
 source /home/ken/.bashrc
 source ./.env
 
-# build and push to s3
-npm run build && aws s3 sync build/ s3://samson-ocr-web --delete
+# build and push to s3, replace the old files
+npm run build && aws s3 sync build/ s3://$WEB_BUCKET_NAME --delete
 
-# invalidate cloudfront distribution
+# invalidate cloudfront distribution, to refresh cache, and ensure clients get new versions
 aws cloudfront create-invalidation --distribution-id=$DISTRIBUTION_ID --paths "/*"
